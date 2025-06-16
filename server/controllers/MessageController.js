e773d5ecdc959ac0725d9e08dddd41b46a6a414f
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-06-16 17:03:40 +0530

    backend complete all routes and controllers and models done just use that api in frontend and connect to each other

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -1,5 +1,7 @@
 import Message from '../models/Message.js';
 import User from '../models/User.js';
+import cloudinary from '../lib/cloudinary.js';
+import { io, userSocketMap } from '../server.js';
 
 // get all users except the logged in user
 
@@ -35,5 +37,71 @@ export const getUsersForSidebar = async (req, res) => {
   }
 };
 
-
 // get all messages for selected users
+export const getMessage = async (req, res) => {
+  try {
+    const { id: selectedUserId } = req.params;
+    const myId = req.user_id;
+    const message = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: selectedUserId },
+        { senderId: selectedUserId, receiverId: myId },
+      ],
+    });
+    await Message.updateMany(
+      {
+        senderId: selectedUserId,
+        receiverId: myId,
+      },
+      { seen: true }
+    );
+    res.json({ success: true, message });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: 'Error getting messages' });
+  }
+};
+
+// api to mark message as seen using message id
+export const markMessageAsSeen = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    await Message.findOneAndUpdate(userId, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: 'Error getting messages from params' });
+  }
+};
+
+// send message to selected user
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user_id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    // emit the new message to the receiver's socket
+    const receiverSocketId = userSocketMap[receiverId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit('newMessage', newMessage);
+    }
+    res.json({ success: true, newMessage });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: 'Error sending message' });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,30 +1,59 @@
-import express from 'express'
-import cors from 'cors'
-import http from 'http'
-import dotenv from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import dotenv from 'dotenv/config';
 import { connectDB } from './lib/db.js';
-import userRoutes from './routes/userRoutes.js'
-
+import userRoutes from './routes/userRoutes.js';
+import messageRoutes from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
 
 // creating app and HTTP server
-const app = express()
-const server = http.createServer(app)
-dotenv.config()
+const app = express();
+const server = http.createServer(app);
+
+// initializing socket.io
+export const io = new Server(server, {
+  cors: { origin: '*' },
+});
+
+//store online users
+export const userSocketMap = {}; //{userId:socketId}
+
+// io.on [receive event]
+// io.emit [send event]
+
+// Socket.io connection handler
+io.on('connection', (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log(`User ${userId} connected`);
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
+  // emit online users to all connected client
+  io.emit('getOnlineUsers', Object.keys(userSocketMap));
+  socket.on('disconnect', () => {
+    console.log(`User ${userId} disconnected`);
+    delete userSocketMap[userId];
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+  });
+});
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/status', (req, res) => {
-  res.status(200).json({ message: 'Server is live..' })
-})
-app.use("/api/auth", userRoutes)
+  res.status(200).json({ message: 'Server is live..' });
+});
+app.use('/api/auth', userRoutes);
+app.use('/api/message', messageRoutes);
 
 // connecting to DB
-connectDB()
+connectDB();
 
 app.listen(5000, () => {
   console.log(
     `Server is running on http://localhost:${process.env.PORT || 5000}`
-  )
-})
+  );
+});
